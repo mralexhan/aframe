d0ab7d4e403415c18cacc2d3ef20e73c3c19d935
mralexhan <aalleexx2211@gmail.com>
2024-11-13 19:19:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ window.onload = () => {
             entity.setAttribute('gltf-model', '#object');
             entity.setAttribute('scale', '25 25 25');
             entity.setAttribute('rotation', '0 90 0');
-            entity.setAttribute('position', '0 -0.4 10');
+            entity.setAttribute('position', '0 -5 10');
             entity.setAttribute('gps-new-entity-place', {
                 latitude: e.detail.position.latitude + 0.001,
                 longitude: e.detail.position.longitude
