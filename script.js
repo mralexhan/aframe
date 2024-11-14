a6fe7d9ab7de3431d7438def8090f27fcaf7fd2e
mralexhan <aalleexx2211@gmail.com>
2024-11-14 09:41:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ window.onload = () => {
             entity.setAttribute('rotation', '0 90 0');
             entity.setAttribute('position', '0 -50 0');
             entity.setAttribute('gps-new-entity-place', {
-                latitude: e.detail.position.latitude - 0.5,
+                latitude: e.detail.position.latitude - 1,
                 longitude: e.detail.position.longitude
             });
             document.querySelector("a-scene").appendChild(entity);
